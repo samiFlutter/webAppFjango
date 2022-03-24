c18fe556f1641cc2ac7ae1b52b58cdf544d4457d
sami <bellalzitouni@gmail.com>
2022-03-24 22:15:31 +0100

    removing Postlist change the date format adding blogs to admin page an edut them

--- django_admin/blog/views.py
+++ django_admin/blog/views.py
@@ -1,25 +1,25 @@
 from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post 
 
-
-posts =[
-    {
-        'author':'CoreyMS',
-        'title':'Blog Post 1',
-        'content':'First post content',
-        'date_posted':'August 27,2018',
-    },
-    {
-        'author':'Jane Doe',
-        'title':'Blog Post 2',
-        'content':'Second post content',
-        'date_posted':'August 28,2018',
-    }
-]
+# posts =[
+#     {
+#         'author':'CoreyMS',
+#         'title':'Blog Post 1',
+#         'content':'First post content',
+#         'date_posted':'August 27,2018',
+#     },
+#     {
+#         'author':'Jane Doe',
+#         'title':'Blog Post 2',
+#         'content':'Second post content',
+#         'date_posted':'August 28,2018',
+#     }
+# ]
 def home(request):
     context ={
-        'posts':posts
+        'posts':Post.objects.all()
     }
     # return HttpResponse('<h1>blog home</h1>')
     return render(request,'blog/home.html',context)
