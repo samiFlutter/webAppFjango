a68f7b95d511eef775d4e77d911d5c8d37ab8069
sami <bellalzitouni@gmail.com>
2022-03-25 01:51:01 +0100

    after register form

--- django_admin/blog/models.py
+++ django_admin/blog/models.py
@@ -6,6 +6,6 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now )
     author = models.ForeignKey(User,on_delete=models.CASCADE)
-    
+    # to get the title in the query 
     def __str__(self) :
         return self.title
